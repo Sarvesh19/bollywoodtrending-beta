842a13252e1c1de005e848fbef7994c86975fc4c
SARVESH YADAV <sarveshyadav@SARVESHs-MacBook-Air.local>
2020-07-06 23:12:20 +0530

    aterial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AngularMaterialModule } from './angular-module/material-module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    AngularMaterialModule,
     AppRoutingModule
   ],
   providers: [],
